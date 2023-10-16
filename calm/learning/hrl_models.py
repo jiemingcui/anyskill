bcec0365e4888a392eadd1a2c788bafde87a8628
ln23415 <ln23415@hotmail.com>
2023-10-16 21:56:47 +0800

    add support for multi-texts training

--- calm/learning/hrl_models.py
+++ calm/learning/hrl_models.py
@@ -44,3 +44,13 @@ class ModelHRLContinuous(ModelA2CContinuousLogStd):
         def __init__(self, a2c_network):
             super().__init__(a2c_network)
             return
+        # def forward(self, input_dict):
+        #     is_train = input_dict.get('is_train', True)
+        #     result = super().forward(input_dict)
+        #
+        #     if (is_train):
+        #         amp_obs = input_dict['amp_obs']
+        #         enc_pred = self.a2c_network.eval_enc(amp_obs)
+        #         result["enc_pred"] = enc_pred
+        #
+        #     return result
